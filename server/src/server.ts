b421183039deead5d4ac286eb86d7b4e3670d467
BadPericles <flaviniska1@gmail.com>
2022-05-25 00:32:26 -0300

    last dance

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,14 +10,14 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const transport = nodemailer.createTransport({
+/*const transport = nodemailer.createTransport({
   host: "smtp.mailtrap.io",
   port: 2525,
   auth: {
     user: "19c2602aef74f8",
     pass: "1a8140206b79b8"
   }
-});
+});*/
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('HTTP server is running!');
